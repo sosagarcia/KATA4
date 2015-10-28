2ed438e9cad17c97b8fbe93da833237fc5e895f6
usuario <usuario@10.22.143.13>
2015-10-28 10:15:30 +0000

    version Rober

---
+++ src/kata4/Histogram.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata4;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ *
+ * @author usuario
+ */
+public class Histogram<T> {
+    HashMap<T, Integer>map = new HashMap<>();
+
+    public Integer get(Object key) {
+        return map.get(key);
+    }
+
+    public Set<T> keySet() {
+        return map.keySet();
+    }
+    public void increment (T key){
+    map.put(key, map.containsKey(key)? map.get(key)+1:1);
+    }
+}
---
+++ src/kata4/MainListReader.java
@@ -0,0 +1,26 @@
+package kata4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainListReader {
+    
+    public static ArrayList <String> reader (String name) throws FileNotFoundException, IOException{
+    ArrayList <String> mailList = new ArrayList<>();
+    
+    BufferedReader read = new BufferedReader(new FileReader(new File(name)));
+    String mail;
+    while((mail = read.readLine())!= null){
+        if (mail.contains("@")){
+            mailList.add(mail);
+        }
+    }
+    return mailList;
+    
+    }
+    
+}
